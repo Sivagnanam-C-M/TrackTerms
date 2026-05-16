afb0172b460eb89a9ab60934698d206c6bfe1ec2
Sivagnanam C M <sivagnanamcm2006@gmail.com>
2026-05-16 22:04:16 +0530

    Changed summarization model

--- summarizer.py
+++ summarizer.py
@@ -1,10 +1,12 @@
 import requests
 import os
 
-API_URL = "https://router.huggingface.co/hf-inference/models/facebook/bart-large-cnn"
+API_URL ="https://router.huggingface.co/hf-inference/models/google/flan-t5-large"
+
 HEADERS = {
     "Authorization": f"Bearer {os.getenv('HF_TOKEN')}"
 }
+
 
 def split_text(text, chunk_size=500):
 
@@ -25,10 +27,6 @@ def split_text(text, chunk_size=500):
 
 def summarize_text(text):
 
-    if not text.strip():
-
-        return "No text found in PDF."
-
     chunks = split_text(text)
 
     summaries = []
@@ -36,56 +34,43 @@ def summarize_text(text):
     for chunk in chunks:
 
         payload = {
-
-            "inputs": chunk,
-
-            "parameters": {
-
-                "max_length": 120,
-
-                "min_length": 30,
-
-                "do_sample": False
-            }
+            "inputs":
+                f"Summarize this terms and conditions document:\n\n{chunk}"
         }
 
+        response = requests.post(
+            API_URL,
+            headers=HEADERS,
+            json=payload
+        )
+
+        print("STATUS:", response.status_code)
+        print("RESPONSE:", response.text)
+
+        if response.status_code != 200:
+            continue
+
         try:
-
-            response = requests.post(
-
-                API_URL,
-
-                headers=HEADERS,
-
-                json=payload,
-
-                timeout=60
-            )
-
-            print("STATUS:", response.status_code)
-            print("TEXT:", response.text)
-
-            if response.status_code != 200:
-
-                continue
 
             result = response.json()
 
             if (
                 isinstance(result, list)
                 and len(result) > 0
-                and "summary_text" in result[0]
+                and "generated_text" in result[0]
             ):
 
                 summaries.append(
-                    result[0]["summary_text"]
+                    result[0]["generated_text"]
                 )
 
         except Exception as e:
 
-            print("ERROR:", e)
+            print("JSON ERROR:", e)
 
-    final_summary = " ".join(summaries)
+    final_summary = " ".join(
+        summaries
+    )
 
     if final_summary.strip() == "":
 
