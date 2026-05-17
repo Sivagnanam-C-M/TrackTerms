db01b1207e4190c464797bf440a40a19189c1027
Sivagnanam C M <sivagnanamcm2006@gmail.com>
2026-05-17 10:24:20 +0530

    Updated summarizer with Groq integration

--- tracker.py
+++ tracker.py
@@ -1,14 +1,10 @@
 import re
 
-
 def clean_text(text):
 
     text = text.replace("\n", " ")
-
     text = re.sub(r'\s+', ' ', text)
-
     return text.strip()
-
 
 def split_sentences(text):
 
@@ -17,35 +13,26 @@ def split_sentences(text):
         text
     )
 
-
 def detect_changes(old_text, new_text):
 
     old_text = clean_text(old_text)
-
     new_text = clean_text(new_text)
-
     old_sentences = split_sentences(old_text)
-
     new_sentences = split_sentences(new_text)
-
     changes = []
 
     for sentence in old_sentences:
-
         if sentence not in new_sentences:
-
             changes.append({
                 "type": "removed",
                 "text": sentence
             })
 
     for sentence in new_sentences:
-
         if sentence not in old_sentences:
-
             changes.append({
                 "type": "added",
                 "text": sentence
             })
-
+            
     return changes
